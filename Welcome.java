6624d1d05685ff68490cd5d2ccc3e425772859e8
SanketPatel77 <sanketpatel4872@gmail.com>
2025-01-07 16:04:42 +0530

    swap code added

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,13 @@
 class Welcome {
     public static void main(String[] args) {
         System.out.println("Welcome to java program");
+
+        // swapping code
+        int a = 10;
+        int b = 20;
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a + " " + b);
     }
 }
